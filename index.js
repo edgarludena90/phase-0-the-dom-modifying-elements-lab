d058b627bbd7c6767f81abb6e32c57e66b532ab5
edgar ludena <edgar.ludena90@gmail.com>
2022-10-04 15:52:16 -0400

    commit tree bb6fe0ff3d90366891bc63e629b6ed86131a25be

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code here!
+// // Write your code here!
+// const element = document.createElement('div')
+
+// document.body.append(element)
+
+// const ul = document.createElement('ul')
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement('li')
+//   li.textContent = (i + 1).toString()
+//   ul.append(li)
+// }
+// element.append(ul)
+
+const newHeader = document.createElement('h1')
+
+newHeader.setAttribute('id', 'victory')
+
+newHeader.innerHTML = "Edagar is the champion"
+document.body.append(newHeader)
